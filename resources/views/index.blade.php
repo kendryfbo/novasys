c174951bb7d23e5fca2e01a6db65c5fc0d3889f1
Kendry <kendryfbo@gmail.com>
2017-03-06 15:14:11 -0300

    Creacion de layout

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,94 +1,79 @@
-<!DOCTYPE html>
+@extends('layouts.master')
 
-<html lang="{{ config('app.locale') }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@section('title', 'Main')
 
-        <title>Main</title>
+@section('content')
+	<hr>
+	<div class="jumbotron col-8 offset-2">
+		<div class="text-center">
+			<h1>Novasys <span class="badge badge-default">2.0</span></h1>
+		</div>
+		<hr>
+		<div class="row">
+			<div class="col-md-4">
+				<div class="card text-center">
+				<a href="#" class="card-link">
+					<img src="images/desarrollo.jpg" alt="Desarrollo" class="rounded mx-auto d-block" style="width: 8rem;">
+					<div class="card-block">
+	    				<h5 class="card-title">Desarrollo</h5>
+	    			</div>
+				</a>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="card text-center">
+				<a href="#" class="card-link">
+					<img src="images/comercial.png" alt="Comercial" class="rounded mx-auto d-block" style="width: 8rem;">
+					<div class="card-block">
+	    				<h5 class="card-title">Comercial</h5>
+	    			</div>
+				</a>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="card text-center">
+				<a href="#" class="card-link">
+					<img src="images/finanzas.png" alt="Finanzas" class="rounded mx-auto d-block" style="width: 8rem;">
+					<div class="card-block">
+	    				<h5 class="card-title">Finanzas</h5>
+	    			</div>
+				</a>
+				</div>
+			</div>
+		</div>	<!-- row -->
+		<br>
+		<div class="row">
+			<div class="col-md-4">
+				<div class="card text-center">
+				<a href="#" class="card-link">
+					<img src="images/operaciones.png" alt="Operaciones" class="rounded mx-auto d-block" style="width: 8rem;">
+					<div class="card-block">
+	    				<h5 class="card-title">Operaciones</h5>
+	    			</div>
+				</a>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="card text-center">
+				<a href="#" class="card-link">
+					<img src="images/calidad.png" alt="Calidad" class="rounded mx-auto d-block" style="width: 8rem;">
+					<div class="card-block">
+	    				<h5 class="card-title">Calidad</h5>
+	    			</div>
+				</a>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="card text-center">
+				<a href="#" class="card-link">
+					<img src="images/informes.png" alt="Informes" class="rounded mx-auto d-block" style="width: 8rem;">
+					<div class="card-block">
+	    				<h5 class="card-title">Informes</h5>
+	    			</div>
+				</a>
+				</div>
+			</div>
+		</div>	<!-- row -->
+	</div>	<!-- Jumbotron -->
 
-        <!-- Styles -->
-        <link rel="stylesheet" href="css/app.css">
-
-    </head>
-    
-    <body>
-    	<hr>
-        <div class="jumbotron col-8 offset-2">
-        	<div class="text-center">
-        		<h1>Novasys <span class="badge badge-default">2.0</span></h1>
-        	</div>
-        	<hr>
-        	<div class="row">
-        		<div class="col-4">
-        			<div class="card text-center">
-        			<a href="#" class="card-link">
-        				<img src="images/desarrollo.jpg" alt="Desarrollo" class="rounded mx-auto d-block" style="width: 8rem;">
-        				<div class="card-block">
-	        				<h6 class="card-title">Desarrollo</h6>
-	        			</div>
-        			</a>
-        			</div>
-        		</div>
-        		<div class="col-4">
-        			<div class="card text-center">
-        			<a href="#" class="card-link">
-        				<img src="images/comercial.png" alt="Comercial" class="rounded mx-auto d-block" style="width: 8rem;">
-        				<div class="card-block">
-	        				<h6 class="card-title">Comercial</h6>
-	        			</div>
-        			</a>
-        			</div>
-        		</div>
-        		<div class="col-4">
-        			<div class="card text-center">
-        			<a href="#" class="card-link">
-        				<img src="images/finanzas.png" alt="Finanzas" class="rounded mx-auto d-block" style="width: 8rem;">
-        				<div class="card-block">
-	        				<h6 class="card-title">Finanzas</h6>
-	        			</div>
-        			</a>
-        			</div>
-        		</div>
-        	</div>	<!-- row -->
-        	<hr>
-        	<div class="row">
-        		<div class="col-4">
-        			<div class="card text-center">
-        			<a href="#" class="card-link">
-        				<img src="images/operaciones.png" alt="Operaciones" class="rounded mx-auto d-block" style="width: 8rem;">
-        				<div class="card-block">
-	        				<h6 class="card-title">Operaciones</h6>
-	        			</div>
-        			</a>
-        			</div>
-        		</div>
-        		<div class="col-4">
-        			<div class="card text-center">
-        			<a href="#" class="card-link">
-        				<img src="images/calidad.png" alt="Calidad" class="rounded mx-auto d-block" style="width: 8rem;">
-        				<div class="card-block">
-	        				<h6 class="card-title">Calidad</h6>
-	        			</div>
-        			</a>
-        			</div>
-        		</div>
-        		<div class="col-4">
-        			<div class="card text-center">
-        			<a href="#" class="card-link">
-        				<img src="images/informes.png" alt="Informes" class="rounded mx-auto d-block" style="width: 8rem;">
-        				<div class="card-block">
-	        				<h6 class="card-title">Informes</h6>
-	        			</div>
-        			</a>
-        			</div>
-        		</div>
-        	</div>	<!-- row -->
-        </div>	<!-- Jumbotron -->
-
-		//  javascript
-        <script src="js/app.js"></script>
-
-     </body> 
-</html>
+@endsection		
