556a0750d249dd6134d4fd6e50df0e1a3b04d924
Kendry <kendryfbo@gmail.com>
2017-03-07 12:01:20 -0300

    Creacion de carpeta view/gerencia

---
+++ resources/views/gerencia/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.master')
+
+@section('title','Gerencia')
+
+@extends('layouts.master')
+
+@section('title', 'Gerencia')
+
+@include('gerencia.layouts.nav')
---
+++ resources/views/gerencia/layouts/nav.blade.php
@@ -0,0 +1,21 @@
+<nav class="navbar fixed-top navbar-toggleable-md navbar-inverse bg-inverse">
+    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+    	<span class="navbar-toggler-icon"></span>
+  	</button>
+
+	<div class="collapse navbar-collapse" id="navbarColor01">
+	  <ul class="navbar-nav">
+	    <li class="nav-item">
+	      <a class="nav-link" href="#">Desarrollo<span class="sr-only">(current)</span></a>
+	    </li>
+	     <li class="nav-item">
+	      <a class="nav-link" href="#">Comercial<span class="sr-only">(current)</span></a>
+	    </li>
+	  </ul>
+	  <ul class="navbar-nav ml-auto">
+	    <li class="nav-item">
+	      <a class="nav-link" href="#"><i class="fa fa-sign-out" aria-hidden="true"></i> Salir<span class="sr-only">(current)</span></a>
+	    </li>
+	  </ul>
+	</div>
+  </nav>
