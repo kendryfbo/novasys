5da2d18d58ee50c113938167db2256f18b5b92dd
Kendry <kendryfbo@gmail.com>
2017-05-15 19:29:20 -0400

    Corrección, se implemento función getAllActive() en familia y se elimino de Tipo de familia #5

--- app/Models/TipoFamilia.php
+++ app/Models/TipoFamilia.php
@@ -6,6 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class TipoFamilia extends Model
 {
+
+	static function getAllActive() {
+
+		return TipoFamilia::All()->where('activo',1);
+	}
+
 	public function familias(){
 
 		return $this->hasMany('App\Models\Familia');
