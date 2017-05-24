f55e1a4b936b3b3bfe1d47d6e22a8c9e2ccc0e1b
Kendry <kendryfbo@gmail.com>
2017-05-24 12:01:56 -0400

    Mejora en SideBar Menu

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -3,29 +3,32 @@
 
   <!-- Sidebar Menu -->
   <ul class="sidebar-menu">
-	<li class="header">Menu</li>
-	<!-- Optionally, you can add icons to the links -->
-	<li class="active"><a href="/desarrollo"><i class="fa fa-link"></i> <span>Desarrollo</span></a></li>
-    <li><a href="/desarrollo/productos"><i class="fa fa-link"></i> <span>Productos</span></a></li>
-    <li><a href="/desarrollo/marcas"><i class="fa fa-link"></i> <span>Marcas</span></a></li>
-    <li><a href="/desarrollo/familias"><i class="fa fa-link"></i> <span>Familias</span></a></li>
-    <li><a href="/desarrollo/sabores"><i class="fa fa-link"></i> <span>Sabores</span></a></li>
-    <li><a href="/desarrollo/formatos"><i class="fa fa-link"></i> <span>Formatos</span></a></li>
-    <li><a href="/desarrollo/premezclas"><i class="fa fa-link"></i> <span>Premezclas</span></a></li>
-    <li><a href="/desarrollo/insumos"><i class="fa fa-link"></i> <span>Insumos</span></a></li>
-    <li><a href="/desarrollo/formulas"><i class="fa fa-link"></i> <span>Formulas</span></a></li>
-    <li><a href="/desarrollo/formulas/autorization"><i class="fa fa-link"></i> <span>Autorizar Formulas</span></a></li>
-
-    <li class="treeview">
-      <a href=""><i class="fa fa-link"></i> <span>SubMenus</span>
+	<li class="header">Desarrollo</li>
+    <li class="treeview menu-open">
+      <a href=""><i class="fa fa-link"></i> <span>Mantencion</span>
         <span class="pull-right-container">
           <i class="fa fa-angle-left pull-right"></i>
         </span>
       </a>
       <ul class="treeview-menu">
-    		<li><a href="#">item 1</a></li>
-            <li><a href="#">item 2</a></li>
-            <li><a href="#">item 3</a></li>
+          <li><a href="/desarrollo/productos"><i class="fa fa-link"></i> <span>Productos</span></a></li>
+          <li><a href="/desarrollo/marcas"><i class="fa fa-link"></i> <span>Marcas</span></a></li>
+          <li><a href="/desarrollo/familias"><i class="fa fa-link"></i> <span>Familias</span></a></li>
+          <li><a href="/desarrollo/sabores"><i class="fa fa-link"></i> <span>Sabores</span></a></li>
+          <li><a href="/desarrollo/formatos"><i class="fa fa-link"></i> <span>Formatos</span></a></li>
+          <li><a href="/desarrollo/premezclas"><i class="fa fa-link"></i> <span>Premezclas</span></a></li>
+          <li><a href="/desarrollo/insumos"><i class="fa fa-link"></i> <span>Insumos</span></a></li>
+      </ul>
+    </li>
+    <li class="treeview">
+      <a href=""><i class="fa fa-link"></i> <span>Movimiento</span>
+        <span class="pull-right-container">
+          <i class="fa fa-angle-left pull-right"></i>
+        </span>
+      </a>
+      <ul class="treeview-menu">
+          <li><a href="/desarrollo/formulas"><i class="fa fa-link"></i> <span>Formulas</span></a></li>
+          <li><a href="/desarrollo/formulas/autorization"><i class="fa fa-link"></i> <span>Autorizar Formulas</span></a></li>
       </ul>
     </li>
   </ul>
