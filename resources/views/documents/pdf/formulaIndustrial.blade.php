b3d222b1414a7b1554b5253d50f3d1f63021b937
Kendry <kendryfbo@gmail.com>
2017-08-11 16:44:40 -0400

    Test Formato PDF Formula Industrial

---
+++ resources/views/documents/pdf/formulaIndustrial.blade.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	  <meta charset="utf-8">
+	  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	  <title>Novasys 2.0</title>
+	  <!-- Tell the browser to be responsive to screen width -->
+	  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+	  <!-- Bootstrap 3.3.7 -->
+	  <link rel="stylesheet" href="{{asset('bootstrap/css/bootstrap.min.css')}}">
+	</head>
+	<body>
+		
+		<div class="container">
+			<div class="title">
+				<h3 class="text-center">Novafoods</h3>
+				<h3 class="text-center"><strong>FORMULA INDUSTRIAL</strong></h3>
+			</div>
+			<table>
+				<tr>
+					<th>FORM-302-05</th>
+				</tr>
+				<tr>
+					<th>Rev. : 01</th>
+				</tr>
+				<tr>
+					<th>Fecha: 28/01/2014</th>
+				</tr>
+			</table>
+			<table class="pull-right">
+					<tr>
+						<th class="text-right">Marca de Producto: </th>
+						<td class="text-right">producto</td>
+					</tr>
+					<tr>
+						<th class="text-right">Sabor: </th>
+						<td class="text-right">producto</td>
+					</tr>
+					<tr>
+						<th class="text-right">Tipo de Producto: </th>
+						<td class="text-right">producto</td>
+					</tr>
+					<tr>
+						<th class="text-right">Formato: </th>
+						<td class="text-right">Fecha</td>
+					</tr>
+					<tr>
+						<th class="text-right">Version: </th>
+						<td class="text-right">producto</td>
+					</tr>
+			</table>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<table>
+					<tr>
+						<th class="">COMPONENTES</th>
+						<th class="text-center">Kg. x ## UNIDADES</th>
+						<th class="text-center">Kg. x 01 UNIDAD</th>
+					</tr>
+
+					<tr>
+						<td>@{{datos}}</td>
+						<td class="text-center">@{{datos}}</td>
+						<td class="text-center">@{{datos}}</td>
+					</tr>
+
+					<tr>
+						<th class="text-right">TOTAL:</th>
+						<th class="text-center">@{{datos}}</th>
+						<th class="text-center">@{{datos}}</th>
+					</tr>
+			</table>
+			<hr>
+			<table>
+					<tr>
+						<th class="">COMPONENTES</th>
+						<th class="text-center">Kg. x ## UNIDADES</th>
+						<th class="text-center">Kg. x 01 UNIDAD</th>
+					</tr>
+
+					<tr>
+						<td>@{{datos}}</td>
+						<td class="text-center">@{{datos}}</td>
+						<td class="text-center">@{{datos}}</td>
+					</tr>
+
+					<tr>
+						<th class="text-right">TOTAL:</th>
+						<th class="text-center">@{{datos}}</th>
+						<th class="text-center">@{{datos}}</th>
+					</tr>
+			</table>
+			<hr>
+			<table>
+					<tr>
+						<th class="">COMPONENTES</th>
+						<th class="text-center">Kg. x ## UNIDADES</th>
+						<th class="text-center">Kg. x 01 UNIDAD</th>
+					</tr>
+
+					<tr>
+						<td>@{{datos}}</td>
+						<td class="text-center">@{{datos}}</td>
+						<td class="text-center">@{{datos}}</td>
+					</tr>
+
+					<tr>
+						<th class="text-right">TOTAL:</th>
+						<th class="text-center">@{{datos}}</th>
+						<th class="text-center">@{{datos}}</th>
+					</tr>
+			</table>
+			<hr>
+			<table>
+					<tr>
+						<th class="">COMPONENTES</th>
+						<th class="text-center">Kg. x ## UNIDADES</th>
+						<th class="text-center">Kg. x 01 UNIDAD</th>
+					</tr>
+
+					<tr>
+						<td>@{{datos}}</td>
+						<td class="text-center">@{{datos}}</td>
+						<td class="text-center">@{{datos}}</td>
+					</tr>
+
+					<tr>
+						<th class="text-right">TOTAL:</th>
+						<th class="text-center">@{{datos}}</th>
+						<th class="text-center">@{{datos}}</th>
+					</tr>
+			</table>
+		</div>
+	</body>
+</html>
