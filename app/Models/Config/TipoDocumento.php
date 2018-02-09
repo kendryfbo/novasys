1e546933a536dfe4808826c9ad2b28cc63b1ee3f
Kendry <kendryfbo@gmail.com>
2018-02-09 11:14:17 -0300

    Improv -> Creacion de constantes Tipo y status de documento

--- app/Models/Config/TipoDocumento.php
+++ app/Models/Config/TipoDocumento.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class TipoDocumento extends Model
 {
-    //
+    protected $table = 'tipo_documento';
+    protected $fillable = ['descripcion', 'activo'];
 }
