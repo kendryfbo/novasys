712e9a0ab39107af023cf3fc5ad62bb0e668aa6f
Kendry <kendryfbo@gmail.com>
2018-03-01 11:31:43 -0300

    implement - Bodega -> función que retorna la existencia de productos en bodega, opcinal recibe bodega ID y Tipo de Producto para filtrar

--- app/Models/Bodega/Bodega.php
+++ app/Models/Bodega/Bodega.php
@@ -4,6 +4,7 @@ namespace App\Models\Bodega;
 
 use DB;
 use App\Models\Bodega\Posicion;
+use App\Models\Bodega\PalletDetalle;
 use App\Models\Insumo;
 use Illuminate\Database\Eloquent\Model;
 
@@ -109,29 +110,54 @@ class Bodega extends Model
     static function getStockFromBodega($bodegaId = NULL,$tipo = NULL) {
 
         $results = [];
-        $query = "SELECT productos.codigo,productos.descripcion, sum(pallet_detalle.cantidad) as cantidad
-                    FROM productos,pallet_detalle
-                    WHERE pallet_detalle.item_id=productos.id AND pallet_detalle.pallet_id";
 
+        $query = "SELECT    bod.id bodega_id,
+                            bod.descripcion bodega,
+                            pos.id pos_id,
+                            CONCAT(pos.bloque ,'-',pos.columna,'-',pos.estante) pos,
+                            pm.descripcion medida,
+                            ps.descripcion status_posicion,
+                            p.id pallet_id,
+                            p.numero pallet_numero,
+                            pd.id pd_id,
+                            pd.fecha_venc fecha_venc,
+                            SUM(pd.cantidad) cantidad,
+                            pd.tipo_id tipo_id,
+                            pd.item_id item_id
 
+                    FROM    bodegas bod,
+                            posicion pos,
+                            posicion_status ps,
+                            pallets p,
+                            pallet_medida pm,
+                            pallet_detalle pd
+
+                    WHERE   pos.bodega_id=bod.id
+                        AND pos.status_id=ps.id
+                        AND pos.pallet_id=p.id
+                        AND p.medida_id=pm.id
+                        AND pd.pallet_id=p.id
+                        AND pos.pallet_id IS NOT NULL ";
+
+        if ($bodegaId) {
+
+            $query = $query . " AND bod.id=" . $bodegaId;
+        }
         if ($tipo) {
-
-            $query = $query . " AND pallet_detalle.tipo_id=".$tipo;
-
+            $query = $query . " AND pd.tipo_id=" . $tipo;
         }
 
-        if($bodegaId) {
-
-            $query = $query . " IN (SELECT pallet_id FROM posicion WHERE bodega_id=".$bodegaId.")";
-        } else {
-            $query = $query . " IN (SELECT pallet_id FROM posicion)";
-        }
-
-         $query = $query . " GROUP BY pallet_detalle.item_id, productos.codigo, productos.descripcion ORDER BY cantidad DESC";
-
-
-        dd($query);
+        $query = $query . "GROUP BY pos,tipo_id,item_id";
+        //$query = $query . "GROUP BY bod.id,bodega,pos_id,medida,status_posicion,pallet_id,pallet_numero,pd_id,fecha_Venc,pos,tipo_id,item_id";
         $results = DB::select(DB::raw($query));
+
+        foreach ($results as $item) {
+
+            $detalle = PalletDetalle::find($item->pd_id);
+            $detalle->producto();
+            $item->producto = $detalle->producto;
+        }
+
         return $results;
     }
 
@@ -159,6 +185,12 @@ class Bodega extends Model
         $results = DB::select(DB::raw($query));
         return $results;
     }
+    /*
+    |
+    | Public Functions
+    |
+    */
+
 
     // Relationships
 
