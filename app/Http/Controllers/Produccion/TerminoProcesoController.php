e7a98287417907150b566b239ec6f6619679155e
Kendry <kendryfbo@gmail.com>
2018-08-30 09:50:55 -0300

    change - Nueva maquina 'U' y visualizar mayor cantidad de terminos de proceso en index

--- app/Http/Controllers/Produccion/TerminoProcesoController.php
+++ app/Http/Controllers/Produccion/TerminoProcesoController.php
@@ -14,7 +14,7 @@ class TerminoProcesoController extends Controller
 {
 
     /* Constantes Temporales hasta implementar tablas */
-    const MAQUINAS = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','Q','R','T'];
+    const MAQUINAS = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','Q','R','T','U'];
     const OPERADORES = [0,1,2,3,4,5,6,7,8,9];
     const CODIGOS = ['Z'];
     const BATCHS = [1,2,3,4,5,6,7,8,9];
@@ -28,7 +28,7 @@ class TerminoProcesoController extends Controller
     public function index()
     {
         $statusPend = StatusDocumento::pendienteID();
-        $producciones = TerminoProceso::with('producto','status')->orderBy('id','DESC')->take(20)->get();
+        $producciones = TerminoProceso::with('producto','status')->orderBy('id','DESC')->take(60)->get();
 
         return view('produccion.terminoProceso.index')->with(['producciones' => $producciones,'statusPend' => $statusPend]);
     }
