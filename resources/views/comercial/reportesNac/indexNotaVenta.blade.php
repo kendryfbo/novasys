86dea7e973a355ddfd831e3341b2935f1cc40880
Kendry <kendryfbo@gmail.com>
2018-09-11 14:15:56 -0300

    new - Inclusion de filtro Vendedor

--- resources/views/comercial/reportesNac/indexNotaVenta.blade.php
+++ resources/views/comercial/reportesNac/indexNotaVenta.blade.php
@@ -26,6 +26,7 @@
 				<input type="hidden" name="cliente" value="{{$cliente}}">
 				<input type="hidden" name="desde" value="{{$desde}}">
 				<input type="hidden" name="hasta" value="{{$hasta}}">
+				<input type="hidden" name="vendedor" value="{{$vendedor}}">
 			</form>
 			<!-- /form -->
 			<!-- form -->
@@ -37,11 +38,20 @@
 				<div class="form-group form-group-sm">
 
 					<label class="control-label col-lg-1">Cliente:</label>
-					<div class="col-lg-3">
+					<div class="col-lg-5">
 						<select class="selectpicker" data-width="100%" data-live-search="true" data-style="btn-sm btn-default" name="cliente">
 							<option value="">Todos...</option>
 							@foreach ($clientes as $client)
 								<option {{$cliente == $client->id ? 'selected':''}} value="{{$client->id}}">{{$client->descripcion}}</option>
+							@endforeach
+						</select>
+					</div>
+					<label class="control-label col-lg-1">Vendedor:</label>
+					<div class="col-lg-2">
+						<select class="selectpicker" data-width="100%" data-live-search="true" data-style="btn-sm btn-default" name="vendedor">
+							<option value="">Todos...</option>
+							@foreach ($vendedores as $vend)
+								<option {{$vendedor == $vend->id ? 'selected':''}} value="{{$vend->id}}">{{$vend->nombre}}</option>
 							@endforeach
 						</select>
 					</div>
@@ -87,6 +97,7 @@
 						<th class="text-center">Empresa</th>
 						<th class="text-center">numero</th>
 						<th class="text-center">Cliente</th>
+						<th class="text-center">Vendedor</th>
 						<th class="text-center">Fecha</th>
 						<th class="text-center">Neto</th>
 						<th class="text-center">Total</th>
@@ -99,6 +110,7 @@
 							<td class="text-left">{{$notaVenta->centroVenta->descripcion}}</td>
 							<td class="text-center"><a href="{{route('verNotaVenta',['numero' => $notaVenta->numero])}}" target="_blank"><strong>{{$notaVenta->numero}}</strong></a></td>
 							<td class="text-left">{{$notaVenta->cliente->descripcion}}</td>
+							<td class="text-left">{{$notaVenta->vendedor->nombre}}</td>
 							<td class="text-center">{{$notaVenta->fecha_emision}}</td>
 							<td class="text-right">{{'$ ' . number_format($notaVenta->neto,2,",",".")}}</td>
 							<td class="text-right">{{'$ ' . number_format($notaVenta->total,2,",",".")}}</td>
