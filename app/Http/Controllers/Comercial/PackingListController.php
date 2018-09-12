4563ea215530fe0e1162373580bec0d8b186eba5
Kendry <kendryfbo@gmail.com>
2018-09-12 11:50:45 -0300

    fix - calculo de peso y volumen

--- app/Http/Controllers/Comercial/PackingListController.php
+++ app/Http/Controllers/Comercial/PackingListController.php
@@ -27,9 +27,24 @@ class PackingListController extends Controller
 
     $guiaID = $request->guia;
     $numero = $request->factura;
-    
+
     $factura = FacturaIntl::with('detalles')->where('numero',$numero)->first();
     $guia = GuiaDespacho::with('detalles.producto.formato')->where('id',$guiaID)->first();
+
+    $pesoNetoTotal = 0;
+    $pesoBrutoTotal = 0;
+    $pesoVolumenTotal = 0;
+
+    foreach ($guia->detalles as $detalle) {
+        $pesoNetoTotal += $detalle->cantidad * $detalle->producto->peso_neto;
+        $pesoBrutoTotal += $detalle->cantidad * $detalle->producto->peso_bruto;
+        $pesoVolumenTotal += $detalle->cantidad * $detalle->producto->volumen;
+
+    }
+
+    $guia->peso_neto_total = $pesoNetoTotal;
+    $guia->peso_bruto_total = $pesoBrutoTotal;
+    $guia->volumen_total = $pesoVolumenTotal;
 
     if (!$factura) {
         dd('Factura # '. $numero . ' No Existe.');
