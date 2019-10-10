464a03eb9f39da927f9c41690d48861edcdf3664
FelipeCarrascoCurilen <fcarras@gmail.com>
2019-10-10 12:49:35 -0300

    - Fix indentation for JS file.

--- public/js/comercial/clienteNacional.js
+++ public/js/comercial/clienteNacional.js
@@ -77,23 +77,22 @@ var app = new Vue({
 
 		// Valida el RUT con su cadena completa  "XXXXXXXX-X"
 		validaRut: function(rutCompleto) {
-		rutCompleto = rutCompleto.replace("‐","-");
-		if (!/^[0-9]+[-|‐]{1}[0-9kK]{1}$/.test( rutCompleto ))
-			return false;
-		var tmp 	= rutCompleto.split('-');
-		var digv	= tmp[1];
-		var rut 	= tmp[0];
-		if ( digv == 'K' ) digv = 'k' ;
+			rutCompleto = rutCompleto.replace("‐","-");
+			if (!/^[0-9]+[-|‐]{1}[0-9kK]{1}$/.test( rutCompleto ))
+				return false;
+				var tmp 	= rutCompleto.split('-');
+				var digv	= tmp[1];
+				var rut 	= tmp[0];
+			if ( digv == 'K' ) digv = 'k' ;
+				return (this.dv(rut) == digv );
+		},
 
-		return (this.dv(rut) == digv );
-	},
-	dv : function(T){
-		var M=0,S=1;
-		for(;T;T=Math.floor(T/10))
-			S=(S+T%10*(9-M++%6))%11;
-		return S?S-1:'k';
-	},
-
+		dv : function(T){
+			var M=0,S=1;
+			for(;T;T=Math.floor(T/10))
+				S=(S+T%10*(9-M++%6))%11;
+				return S?S-1:'k';
+		},
 
 		isNumber: function isNumber(n){
     		return typeof(n) != "boolean" && !isNaN(n);
